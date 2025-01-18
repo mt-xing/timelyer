4477c3a49635824dff44312321e7347475e7775a
mt-xing <mt-xing@users.noreply.github.com>
2025-01-18 18:45:46 -0500

    Highlight calendar

--- generate_calendar.js
+++ generate_calendar.js
@@ -13,7 +13,41 @@ function generateWeekOffsetString(weekOffset) {
     return ` + ${weekOffset * 7}`;
 }
 
-function generateDateWrap(weekday, weekOffset, inner) {
+function generateDateWrap(weekday, weekOffset, inner, blueColor, invertColor) {
+    if (invertColor) {
+        console.log(
+`      <Condition>
+        <Expressions>
+          <Expression name="is-day-${weekday}">
+            (([DAY_OF_WEEK] + 5) % 7) == ${weekday}
+          </Expression>
+        </Expressions>
+        <Compare expression="is-day-${weekday}">
+          <PartText x="${startX + width * weekday}" y="${height * (weekOffset + midY)}" width="${width}" height="${height}">
+            <Text align="CENTER">
+              <Font family="SYNC_TO_DEVICE" size="24" color="#000000">
+                <Template>%s
+                  <Parameter expression="${inner}" />
+                </Template>
+              </Font>
+            </Text>
+          </PartText>
+        </Compare>
+        <Default>
+          <PartText x="${startX + width * weekday}" y="${height * (weekOffset + midY)}" width="${width}" height="${height}">
+            <Text align="CENTER">
+              <Font family="SYNC_TO_DEVICE" size="24" color="#ffffffff">
+                <Template>%s
+                  <Parameter expression="${inner}" />
+                </Template>
+              </Font>
+            </Text>
+          </PartText>
+        </Default>
+      </Condition>`
+        );
+        return;
+    }
     console.log(
 `      <PartText x="${startX + width * weekday}" y="${height * (weekOffset + midY)}" width="${width}" height="${height}">
         <Text align="CENTER">
@@ -41,6 +75,15 @@ function generateLine(y) {
     );
 }
 
+console.log(
+`      <PartDraw x="0" y="72" width="450" height="36">
+        <Rectangle x="0" y="0" width="48" height="36">
+          <Transform target="x" value="round(57 + ((([DAY_OF_WEEK] + 5) % 7) * 48))" />
+          <Fill color="#00ffff" />
+        </Rectangle>
+      </PartDraw>`
+);
+
 generateDateWrap(0, -2, "M");
 generateDateWrap(1, -2, "T");
 generateDateWrap(2, -2, "W");
@@ -53,7 +96,7 @@ generateLine(height);
 
 for (let week = -1; week <= 1; week++) {
     for (let day = 0; day < 7; day++) {
-        generateDateWrap(day, week, generateDate(day, week));
+        generateDateWrap(day, week, generateDate(day, week), undefined, week === 0);
     }
     generateLine(height * (week + 3));
 }
