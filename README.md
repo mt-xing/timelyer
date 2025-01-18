f88f6c5bd9a5be56deec8c9b2f50bbd558b1fb9d
mt-xing <mt-xing@users.noreply.github.com>
2025-01-18 15:24:37 -0500

    Delete Everything

--- README.md
+++
@@ -1,6 +0,0 @@
-# timelyer
-A remake of the legendary Timely Pebble watchface, but for wear OS.
-
-Originaly Timely:
-
-https://github.com/cynorg/PebbleTimely
--- app/src/main/java/com/michaelxing/timelyer/data/watchface/ArmDimensions.kt
+++
@@ -1,8 +0,0 @@
-package com.michaelxing.timelyer.data.watchface
-
-data class ArmDimensions(
-    val lengthFraction: Float,
-    val widthFraction: Float,
-    val xRadiusRoundedCorners: Float,
-    val yRadiusRoundedCorners: Float
-)
